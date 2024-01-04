b8b1784d85574ba3619cf67e238b98007c9e04b7
Dark Overlord <111354371+darklyonteam@users.noreply.github.com>
2024-01-04 16:43:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 module.exports = {
-TOKEN: "",
-ownerID: [""], //write your discord user id. example: ["id"] or ["id1","id2"]
-botInvite: "", //write your discord bot invite.
-supportServer: "", //write your discord bot support server invite.
-mongodbURL: "", //write your mongodb url.
-status: '❤️ codeshare.me',
+TOKEN: "MTE5MjQwNDc2NzkzNTMxMTk1Mw.Gi-nfq.lGGCs-RJ_AgOOg-76GSrsm1Gbyh-2QiWSIS5B0",
+ownerID: ["1153172663838195724"], //write your discord user id. example: ["id"] or ["id1","id2"]
+botInvite: "https://discord.com/api/oauth2/authorize?client_id=1192404767935311953&permissions=8&redirect_uri=https%3A%2F%2Fkgufycfdmjhvyut-f673959e18a8.herokuapp.com%2F&scope=bot", //write your discord bot invite.
+supportServer: "idk", //write your discord bot support server invite.
+mongodbURL: "mongodb+srv://indumina1:indumina1@cluster0.y0pxmkf.mongodb.net/?retryWrites=true&w=majority", //write your mongodb url.
+status: '❤️ imsus',
 commandsDir: './commands', //Please don't touch
 language: "en", //en, tr, nl, pt, fr, ar, zh_TW, it, ja
 embedColor: "ffa954", //hex color code
-errorLog: "", //write your discord error log channel id.
+errorLog: "opps", //write your discord error log channel id.
 
 
 sponsor: {
-status: true, //true or false
-url: "https://awmbilisim.com", //write your discord sponsor url.
+status: false, //true or false
+url: "idk", //write your discord sponsor url.
 },
 
 voteManager: { //optional
